2818015477ef4c39814e0f872688a3ef34b94f10
Jakub Czachor <42331274+JakubCzachor@users.noreply.github.com>
2019-03-13 12:31:56 -0400

    Add files via upload

---
+++ transcriptase.cpp
@@ -0,0 +1,61 @@
+
+/*
+ Author: Jakub Czachor
+ Course: CSCI-136
+ Instructor: Anoop Aroor
+ Assignment: Project 21
+ This program takes a string of DNA and prints the RNA complements
+ */
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <fstream>
+
+using namespace std;
+char DNAbase_to_mRNAbase(char x)
+{
+    char z = toupper(x); //Makes letters capital
+    char comp; //New Char
+    if (z == 'A')
+    {
+        comp = 'U';
+    }
+    else if (z == 'T'){
+        comp = 'A';
+    }
+    else if (z == 'C'){
+        comp = 'G';
+    }
+    else{
+        comp = 'C';
+    }
+    return comp; //The character returned is the complement
+}
+string DNA_to_mRNA(string dna)
+{
+    string dna0; //New string
+    for(int i = 0; i < dna.length(); i++) //For every character
+    {
+        char char0 = dna[i];
+        dna0 += DNAbase_to_mRNAbase(char0);
+    }
+    return dna0;
+}
+int main() {
+    
+    ifstream fin("dna.txt");
+    if (fin.fail())
+    {
+        cerr << "File cannot be read, opened, or does not exist.\n";
+        exit(1);
+    }
+    string strand;
+    while(getline(fin, strand)) //Prints out for multiple lines of DNA strands
+    {
+        cout << DNA_to_mRNA(strand) << endl;
+    }
+    fin.close();
+}
+
